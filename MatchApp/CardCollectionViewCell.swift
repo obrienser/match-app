fb32329892e22871f8b75ca4764dbf5decb0e942
Obrienser <obrienser@gmail.com>
2021-07-14 03:18:44 -0700

    Add new CardCollectionView

---
+++ MatchApp/CardCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CardCollectionViewCell.swift
+//  MatchApp
+//
+//  Created by Сергей Масленников on 14.07.2021.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+}
