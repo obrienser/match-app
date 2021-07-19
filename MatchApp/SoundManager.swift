ad6241490c35c202c65826bea0b168068530e679
Obrienser <obrienser@gmail.com>
2021-07-19 03:28:46 -0700

    Create Sound manager

---
+++ MatchApp/SoundManager.swift
@@ -0,0 +1,21 @@
+//
+//  SoundManager.swift
+//  MatchApp
+//
+//  Created by Сергей Масленников on 19.07.2021.
+//
+
+import Foundation
+import AVFoundation
+
+class SoundManager {
+    
+    var audioPlayer: AVAudioPlayer?
+    
+    func playSound() {
+        
+        
+        
+    }
+    
+}
