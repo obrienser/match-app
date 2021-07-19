89ca49ecefa145f777c3b9d77ea151b6636818b8
Obrienser <obrienser@gmail.com>
2021-07-19 04:27:22 -0700

    Final Audio

--- MatchApp/SoundManager.swift
+++ MatchApp/SoundManager.swift
@@ -21,7 +21,48 @@ class SoundManager {
     
     func playSound(effect: SoundEffect) {
         
+        var soundFilename = ""
         
+        switch effect {
+            
+        case .flip:
+            soundFilename = "cardflip"
+            
+        case .match:
+            soundFilename = "dingcorrect"
+            
+        case .nomatch:
+            soundFilename = "dingwrong"
+            
+        case .shuffle:
+            soundFilename = "shuffle"
+            
+        }
+        
+        // Get the path to the resource
+        let bundlePath = Bundle.main.path(forResource: soundFilename, ofType: ".wav")
+        
+        // Check that it`s not nil
+        guard bundlePath != nil else {
+            // Couldn`t find the resource, exit
+            return
+        }
+        
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        do {
+            
+            // Create the audio player
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            
+            // Play the sound effect
+            audioPlayer?.play()
+            
+        }
+        catch {
+            print("Couldn`t create an audio player")
+            return
+        }
         
     }
     
